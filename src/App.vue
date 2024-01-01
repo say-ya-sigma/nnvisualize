32f8de15df7cc82b22337aa72c28c8197a059eff
sigma <seiya.sugimoto.free@gmail.com>
2024-01-01 22:40:24 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <h1>Vue 3 + TypeScript + Vite</h1>
+  </header>
+
+  <main>
+    hello world
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+})
+</script>
